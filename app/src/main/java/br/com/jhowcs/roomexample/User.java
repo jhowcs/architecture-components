c2f5634532f6fd0b2f0fbd8e3803d81591a8419e
Jonathan Campos <jhowcs@gmail.com>
2017-09-09 11:12:31 -0300

    Add setup Room classes

---
+++ app/src/main/java/br/com/jhowcs/roomexample/User.java
@@ -0,0 +1,50 @@
+package br.com.jhowcs.roomexample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/br/com/jhowcs/roomexample/UserDao.java
@@ -0,0 +1,30 @@
+package br.com.jhowcs.roomexample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM user")
+    List<User> getAll();
+
+    @Query("SELECT * FROM user WHERE id = :id")
+    List<User> getById(int id);
+
+    @Query("SELECT * FROM User WHERE first_name LIKE :names OR last_name LIKE :names ")
+    List<User> getByNames(String names);
+
+    @Query("SELECT MAX(id) FROM user")
+    int maxId();
+
+    @Insert
+    void insert(User user);
+
+    @Delete
+    void delete(User user);
+}
