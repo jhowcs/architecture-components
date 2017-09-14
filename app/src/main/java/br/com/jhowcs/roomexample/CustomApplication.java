aa477e6e30a8bafbb8398017ef06b2f00581c3db
Jonathan Campos <jhowcs@gmail.com>
2017-09-13 22:18:05 -0300

    Add initialization of AppExecutor

--- app/src/main/java/br/com/jhowcs/roomexample/CustomApplication.java
+++ app/src/main/java/br/com/jhowcs/roomexample/CustomApplication.java
@@ -2,6 +2,9 @@ package br.com.jhowcs.roomexample;
 
 import android.app.Application;
 
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 import br.com.jhowcs.roomexample.repository.local.DatabaseProvider;
 
 
@@ -11,5 +14,9 @@ public class CustomApplication extends Application {
     public void onCreate() {
         super.onCreate();
         DatabaseProvider.init(this);
+
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        AppExecutor.init(availableProcessors, availableProcessors * 2, 0,
+                TimeUnit.SECONDS, new LinkedBlockingDeque());
     }
 }
